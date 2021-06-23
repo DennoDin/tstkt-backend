5f0ff6b7db944890c052f7f49b5449cc4c0343aa
Dustin Tran <dustin.andy.tran@gmail.com>
2021-06-23 15:40:25 +0900

    WIP: Succesful EventSub Verification created
    Co-authored-by: Tsubasa Kondo <TsubasaK111@users.noreply.github.com>

--- cheese.js
+++ cheese.js
@@ -3,10 +3,22 @@ require('dotenv').config();
 const {PORT} = process.env || 9999;
 
 const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send(200)
+})
 
 app.get('/api', (req, res) => {
     res.send("Sup Bruh");
 });
+
+app.post('/auth', (req, res) => {
+    // TODO: Verify signature
+    // req.get('Twitch-Eventsub-Message-Signature');
+
+    res.send(req.body.challenge);
+})
 
 app.listen(PORT, () => {
     console.log(`app listening on http://localhost:${PORT}`)
