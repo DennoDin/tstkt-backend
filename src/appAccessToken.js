bade741b219dc97206ef9235e120600e33e0fe7b
Dustin Tran <dustin.andy.tran@gmail.com>
2021-07-07 11:15:12 +0900

    FEAT: Retrieve App Access Token from Twitch
    Co-authored-by: Tsubasa Kondo <TsubasaK111@users.noreply.github.com>

--- src/appAccessToken.js
+++ src/appAccessToken.js
@@ -1,13 +1,27 @@
 const axios = require('axios');
 const url = require('url');
+require('dotenv');
 
-function getAppAccessToken(){
-    const params = new url.URLSearchParams({"test": "hi"});
-    axios.post(
-        "https://id.twitch.tv/oauth2/token", 
-        params.toString(), 
-        (res) => console.log(res)
-    )
+async function getAppAccessToken(){
+    const grant_type = `client_credentials`;
+    const scope = `openid channel:manage:redemptions`
+    const client_id = process.env.CLIENT_ID;
+    const client_secret = process.env.CLIENT_SECRET;
+    const params = new url.URLSearchParams({client_id, client_secret, grant_type, scope});
+    
+    let result;
+    
+    try {
+        result = await axios.post(
+            "https://id.twitch.tv/oauth2/token", 
+            params.toString(), 
+            (res) => console.log(res)
+        )
+    
+    } catch (error) {
+    
+    }
+
     return "appAccessToken"
 }
 
